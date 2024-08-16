acaf8f21c28aba3989800061e688f6660cfacea0
Samuel Teixeira <steixeira1337@gmail.com>
2024-08-16 18:32:09 -0300

    feat(auth): create service of authentication and all of utils

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
+import { User } from '../users/entities/user.entity'; // Importando a entidade User
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(
+    username: string,
+    pass: string,
+  ): Promise<Omit<User, 'password'> | null> {
+    const user = await this.usersService.findOneByUsername(username);
+
+    if (!user || typeof user.password !== 'string') {
+      return null;
+    }
+    const isMatch = await bcrypt.compare(pass, user.password);
+    if (isMatch) {
+      const { password, ...result } = user;
+      return result;
+    }
+
+    return null;
+  }
+
+  async login(user: User) {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
